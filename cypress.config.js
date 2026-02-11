aaf1b487b1ad5074d8b87b05c95319c4501f8d56
QA Automation <qa@automation.com>
2026-02-11 10:36:16 -0300

    Atualiza configuração e nome do teste

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    specPattern: 'cypress/teste-e2e/**/*.cy.js',
+    specPattern: 'cypress/**/*.cy.js',
     baseUrl: 'https://www.automationpratice.com.br',
     setupNodeEvents(on, config) {
       // implement node event listeners here
