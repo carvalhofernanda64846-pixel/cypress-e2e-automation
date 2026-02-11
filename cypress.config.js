ca8277b6ca2e05989da80110005515561375e5ff
QA Automation <qa@automation.com>
2026-02-11 11:20:47 -0300

    atualizaçao das pastas

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,15 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    specPattern: 'cypress/**/*.cy.js',
+    specPattern: ['cypress/teste-e2e/**/*.cy.js', 'cypress/e2e/1-getting-started/**/*.cy.js'],
     baseUrl: 'https://www.automationpratice.com.br',
+    defaultCommandTimeout: 15000,
+    requestTimeout: 15000,
+    responseTimeout: 15000,
+    chromeWebSecurity: false,
+    video: false,
+    screenshotOnRunFailure: true,
+    trashAssetsBeforeRuns: true,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
